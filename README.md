873c8210f64df5ac669626862f7d53ae4d78ad77
esytastic <esytastic@gmail.com>
2023-12-03 14:16:41 +0100

    add react navigation v6

---
+++ README.md
@@ -0,0 +1,1 @@
+# Native project for map app
---
+++ components/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+export class HomeScreen extends Component {
+  render() {
+    return (
+      <Text>Hello</Text>
+    )
+  }
+}
+
+export default HomeScreen 
