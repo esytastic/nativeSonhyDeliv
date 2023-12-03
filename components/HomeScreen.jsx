ef081500014ff1ef51ebb79bb45f23430ca53b3d
esytastic <esytastic@gmail.com>
2023-12-03 15:50:01 +0100

    add Image tag

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -1,14 +1,25 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React, { Component, useLayoutEffect } from 'react'
+import { Text, View, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export class HomeScreen extends Component {
-  render() {
-    return (
-      <View className="bg-red-200">
-        <Text className="text-blue-500">Hello</Text>
-      </View>
-    )
-  }
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      // headerTitle: "TEST"
+      headerShown: false
+    });
+  }, []);
+
+  return (
+
+    <SafeAreaView className="flex-row pb-3 items-center space-x-2">
+      <Image source={{ uri: 'https://www.lotus-qa.com/wp-content/uploads/2020/02/testing.jpg' }} className='h-7 w-7 bg-gray-300 p-4 rounded-full' />
+      <Text className="text-red-300 bg-gray-300">Lorem ipsum dolor sit amet.</Text>
+    </SafeAreaView>
+  )
 }
 
 export default HomeScreen 
