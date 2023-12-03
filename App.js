9a73a25d922f43692e6cd4026bb41260d01321c1
esytastic <esytastic@gmail.com>
2023-12-03 13:36:19 +0100

    add all

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1">
+      <Text className="bg-blue-500">Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
